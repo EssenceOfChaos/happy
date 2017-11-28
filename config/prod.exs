8b4bcad90e182f365c5f9805087b1436f1a61953
EssenceOfChaos <fjschiller@gmail.com>
2017-11-27 21:14:09 -0500

    fix prod.exs to deploy to heroku

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,7 @@ config :happy, HappyWeb.Endpoint,
 
 
   # Configure your database
-  config :conflux, Happy.Repo,
+  config :happy, Happy.Repo,
     adapter: Ecto.Adapters.Postgres,
     url: System.get_env("DATABASE_URL"),
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
